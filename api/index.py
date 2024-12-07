40f19007dc3efb9d5443458197e8e58cdaddbc48
Welington <wp22santos@gmail.com>
2024-12-07 00:55:15 -0300

    refactor: simplifica ao máximo para testar deploy

--- api/index.py
+++ api/index.py
@@ -1,18 +1,10 @@
 from flask import Flask
-import sys
-import os
 
-# Adiciona o diretório pai ao PATH para poder importar o app
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+app = Flask(__name__)
 
-from app import app
+@app.route('/')
+def home():
+    return 'Hello World'
 
-# Configuração para Vercel
-app.debug = False
-
-def handler(request):
-    """Handle a request to the Flask app."""
-    return app.wsgi_app(request.environ, request.start_response)
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
