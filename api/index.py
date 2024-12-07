b16f6211723cbb1f430fdbf1cad5db144dff280b
Welington <wp22santos@gmail.com>
2024-12-07 00:50:50 -0300

    fix: corrige configuração para deploy na Vercel

--- api/index.py
+++ api/index.py
@@ -1,4 +1,12 @@
+from flask import Flask
 from app import app
 
 # Vercel precisa desta variável
 app.debug = False
+
+# Vercel handler
+def handler(request, context):
+    return app(request)
+
+if __name__ == "__main__":
+    app.run()
